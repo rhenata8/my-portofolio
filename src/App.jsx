8757c7643dcb6847d4b5288fde99e67f35d3e1c4
rhenata8 <rhenadvan@gmail.com>
2025-08-04 15:50:19 +0700

    warna

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+// src/App.jsx
+
 import React from 'react';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 
 function App() {
   return (
-    <div className="bg-white text-gray-900">
+    // Latar belakang utama bisa diatur di sini jika ada bagian lain selain Hero
+    <div className="bg-brand-pink"> 
       <Navbar />
-      <Hero />
+      <main>
+        <Hero />
+        {/* Contoh section lain dengan warna latar berbeda untuk variasi */}
+        {/* <div id="about" className="min-h-screen bg-white">About Section</div> */}
+        {/* <div id="projects" className="min-h-screen bg-brand-pink">Projects Section</div> */}
+      </main>
     </div>
   );
 }
